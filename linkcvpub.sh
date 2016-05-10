46334ae8f8b8d0419bf2a3738028cb587d554c9f
a.succurro-donquijote <a.succurro@gmail.com>
2016-05-10 11:58:49 +0200

    update

--- linkcvpub.sh
+++ linkcvpub.sh
@@ -7,5 +7,7 @@ case $yn in
 	[Yy]* ) cd contents && pdflatex extra.tex && cp extra.pdf ../succurro_publications.pdf && cd .. ;;
 esac
 
-pdftk antonellasuccurro_CV_academic.pdf succurro_publications.pdf cat output succurro_CV.pdf
-pdftk antonellasuccurro_CV_linkedin.pdf succurro_publications.pdf cat output succurro_CV_web.pdf
+#pdftk antonellasuccurro_CV_academic.pdf succurro_publications.pdf cat output succurro_CV.pdf
+#pdftk antonellasuccurro_CV_linkedin.pdf succurro_publications.pdf cat output succurro_CV_web.pdf
+qpdf --empty --pages antonellasuccurro_CV_academic.pdf 1-z succurro_publications.pdf 1-z -- succurro_CV.pdf
+qpdf --empty --pages antonellasuccurro_CV_linkedin.pdf 1-z succurro_publications.pdf 1-z -- succurro_CV.pdf
